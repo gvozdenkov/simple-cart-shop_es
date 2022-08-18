72acbefd437d652d8bea144db3cc397f4d0caba2
arty gvozdenkov <gvozdenkov@gmail.com>
2022-08-18 12:15:34 +0300

    feat: finish header logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,17 @@
 import React from "react";
 import "./Header.css";
+import logo from "../../images/logo.png";
 
 function Header() {
-  return <header className="header page__section">Header</header>;
+  return (
+    <header className="header page__section">
+      <img
+        src={logo}
+        alt="Dailyland milk products logo"
+        className="header__logo"
+      />
+    </header>
+  );
 }
 
 export default Header;
