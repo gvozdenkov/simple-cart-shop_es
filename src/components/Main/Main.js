aac067c789fd99e0e7ec399fde75575d53ad79f3
arty gvozdenkov <gvozdenkov@gmail.com>
2022-08-18 12:49:05 +0300

    feat: add main title

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,7 +2,11 @@ import React from "react";
 import "./Main.css";
 
 function Main() {
-  return <main className="main page__section">Main</main>;
+  return (
+    <main className="main page__section">
+      <h1 className="main__title">High quality, local producers</h1>
+    </main>
+  );
 }
 
 export default Main;
