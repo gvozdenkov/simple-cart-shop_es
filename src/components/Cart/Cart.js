8fe5748863e6c13ce310fe573337d91412880c88
arty gvozdenkov <gvozdenkov@gmail.com>
2022-08-31 14:45:17 +0300

    feat: add form in Cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,42 @@
 import React from "react";
 import { useShoppingCart } from "../contexts/CartContext";
+import Form from "../form/Form";
 import OrderList from "../Order/OrderList/OrderList";
 
 function Cart() {
   const { cartItems } = useShoppingCart();
-  return <OrderList items={cartItems} />;
+  return (
+    <>
+      <OrderList items={cartItems} />
+      <Form name="order">
+        <label className="form__field">
+          Name
+          <input
+            type="text"
+            name="name"
+            className="form__input"
+            required
+          ></input>
+        </label>
+        <label className="form__field">
+          Email address
+          <input
+            type="email"
+            name="email"
+            className="form__input"
+            required
+          ></input>
+        </label>
+        <label className="form__field">
+          Telephone number
+          <input type="tel" name="tel" className="form__input" required></input>
+        </label>
+        <button type="submit" className="form__submit">
+          Check out
+        </button>
+      </Form>
+    </>
+  );
 }
 
 export default Cart;
